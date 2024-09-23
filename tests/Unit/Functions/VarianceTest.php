7615c81ca75ad23c5213a27a461eada3b03fa12f
SniiiperZ <cedric1997@hotmail.com>
2024-09-23 14:08:19 +0200

    Add Variance class for calculating the variance of a list

---
+++ tests/Unit/Functions/VarianceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use SniiiperZ\Stats\Functions\Variance;
+
+test("Calcul la variance d'une liste", function (){
+    $list = [0, 2, 4, 6, 8, 10];
+
+    $result = Variance::calculate($list);
+
+    expect($result)->toBe(12.5);
+});
